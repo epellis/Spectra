26598f09dbfdb9e8ab22f778d9d17ebb1a39f44d
Ned <nedellis1234@gmail.com>
2017-07-10 21:37:13 -0500

    Test for future commits via terminal

---
+++ Software/SpotifyConnect/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+void connectToWiFi();
+void authenticateWithSpotify();
+void getTrackInformation();
+void visualizeTrack();
+
+void setup()
+{
+
+}
+
+void loop() {}
+
+void connectToWiFi()
+{
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("AutoConnectAP");
+}
+
+void authenticateWithSpotify()
+{
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("DNSServer CaptivePortal Example");
+
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  webServer.onNotFound([](){
+    webServer.send(200, "text/html", "<h1>Hello World</h1>");
+  })
+  webServer.begin();
+}
